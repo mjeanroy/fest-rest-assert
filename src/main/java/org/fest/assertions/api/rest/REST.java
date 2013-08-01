ad8f71b81bd5cc197c765db395750ec4968d3724
Mickael Jeanroy <mickael.jeanroy@mos.gfit.fr>
2013-08-01 17:39:58 +0200

    json: add 'hasPath' and 'hasPathEqualTo' assertions

--- src/main/java/org/fest/assertions/api/rest/REST.java
+++ src/main/java/org/fest/assertions/api/rest/REST.java
@@ -1,5 +1,6 @@
 package org.fest.assertions.api.rest;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.fest.assertions.util.Cookie;
@@ -48,6 +49,16 @@ public class REST {
 	}
 
 	/**
+	 * Build new cookie assertion object from {@link org.apache.http.HttpResponse} object.
+	 *
+	 * @param response Original response object.
+	 * @return Assertion object.
+	 */
+	public static JsonAssert assertJsonThat(com.ning.http.client.Response response) {
+		return new JsonAssert(extractJson(response));
+	}
+
+	/**
 	 * Extract cookie from {@link org.apache.http.HttpResponse} object.
 	 *
 	 * @param name Name of cookie to extract.
@@ -62,5 +73,20 @@ public class REST {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * Extract json from {@link org.apache.http.HttpResponse} object.
+	 *
+	 * @param response Original response object.
+	 * @return Json response.
+	 */
+	public static String extractJson(com.ning.http.client.Response response) {
+		try {
+			return response.getResponseBody();
+		}
+		catch (IOException ex) {
+			throw new AssertionError("Cannot extract JSON body from response", ex);
+		}
 	}
 }
---
+++ src/test/java/org/fest/assertions/api/rest/JsonAssertTest.java
@@ -0,0 +1,70 @@
+package org.fest.assertions.api.rest;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonAssertTest {
+
+	private String simpleJson;
+	private String nestedJson;
+
+	private JsonAssert simpleJsonAssertion;
+	private JsonAssert nestedJsonAssertion;
+
+	@Before
+	public void setUp() {
+		simpleJson = "{\"id\": 1, \"name\": \"foo\", \"nickname\": \"bar\", \"flag\": true }";
+		nestedJson = "{\"id\": 1, \"name\": { \"firstName\": \"foo\", \"lastName\": \"bar\" }, \"nickname\": \"bar\", \"flag\": false }";
+
+		simpleJsonAssertion = new JsonAssert(simpleJson);
+		nestedJsonAssertion = new JsonAssert(nestedJson);
+	}
+
+	@Test
+	public void test_hasPath() {
+		simpleJsonAssertion.hasPath("id");
+		nestedJsonAssertion.hasPath("name.firstName");
+
+		try {
+			simpleJsonAssertion.hasPath("foo");
+			fail("Expected AssertionError to be thrown");
+		}
+		catch (AssertionError error) {
+			assertThat(error.getMessage()).isEqualTo("Expected path <foo> to be find in json <{\"id\": 1, \"name\": \"foo\", \"nickname\": \"bar\", \"flag\": true }>");
+		}
+
+		try {
+			nestedJsonAssertion.hasPath("name.foo");
+			fail("Expected AssertionError to be thrown");
+		}
+		catch (AssertionError error) {
+			assertThat(error.getMessage()).isEqualTo("Expected path <name.foo> to be find in json <{\"id\": 1, \"name\": { \"firstName\": \"foo\", \"lastName\": \"bar\" }, \"nickname\": \"bar\", \"flag\": false }>");
+		}
+	}
+
+	@Test
+	public void test_isPathEqualTo() {
+		simpleJsonAssertion.isPathEqualTo("id", 1);
+		simpleJsonAssertion.isPathEqualTo("name", "foo");
+		simpleJsonAssertion.isPathEqualTo("flag", true);
+
+		try {
+			simpleJsonAssertion.isPathEqualTo("name", "bar");
+			fail("Expected AssertionError to be thrown");
+		}
+		catch (AssertionError error) {
+			assertThat(error.getMessage()).isEqualTo("Expect path <name> to be <bar> but was <foo>");
+		}
+
+		try {
+			simpleJsonAssertion.isPathEqualTo("flag", false);
+			fail("Expected AssertionError to be thrown");
+		}
+		catch (AssertionError error) {
+			assertThat(error.getMessage()).isEqualTo("Expect path <flag> to be <false> but was <true>");
+		}
+	}
+}
